8dcf18fef6b3b0862a9b21fca78cbd0b7835018e
DaveSturge <david-sturgi@hotmail.com>
2025-08-16 22:45:39 +0100

    Added date validation

--- src/add_expense.py
+++ src/add_expense.py
@@ -1,13 +1,14 @@
 from utils.formatting import print_title, print_time_date, print_footer, type_text, transition, get_user_choice
-from utils.validation import validate_name, validate_date
+from utils.validation import validate_name, validate_date, validate_cost, validate_category
 
 def add_expense():
     print_title("Add  Expense")
     print_time_date()
     expense_name = get_expense_name()
-    date = get_user_choice(" > Date (DD-MM-YYYY): ")
-    cost = get_user_choice(" > Cost             : £")
-    category = get_user_choice(" > Category         : ")
+    expense_date = get_expense_date()
+    expense_cost = get_expense_price()
+    expense_category = get_expense_category()
+    
     
 
 def get_expense_name():
@@ -17,13 +18,38 @@ def get_expense_name():
         name = get_user_choice(" > Item Name        : ")
         validated = validate_name(name)
 
-    return name
+    return name.strip()
 
 def get_expense_date():
     validated = False
+    date_object = None
 
     while not validated:
         date = get_user_choice(" > Date (DD-MM-YYYY): ")
-        validated = validate_date(date)
+        date_object, error_code = validate_date(date)
 
-    return date
+        if date_object != None:
+            validated = True
+        else:
+            type_text(error_code)
+
+    return date_object
+
+def get_expense_price():
+    validated = False
+
+    while not validated:
+        cost = get_user_choice(" > Cost             : £")
+        validated, decimal_cost = validate_cost(cost)
+
+    return decimal_cost
+
+def get_expense_category():
+    validated = False
+
+    while not validated:
+        category = get_user_choice(" > Category         : ")
+        validated = validate_cost(category)
+
+    return category
+    
--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,5 +1,6 @@
 from utils.formatting import type_text
 from datetime import datetime
+from decimal import Decimal
 
 def validate_name(name):
     if not name.strip():   
@@ -13,6 +14,51 @@ def validate_name(name):
     return True
 
 def validate_date(date):
+    error = "\n--------------Invalid Date--------------"
+
     if not date.strip():   
-        type_text("\n----------Date cannot be empty----------")
-        return False
+        error = "\n----------Date cannot be empty----------"
+        return None, error
+
+    valid_formats = ["%d/%m/%Y", "%d-%m-%Y", "%d/%m/%y", "%d-%m-%y", "%d/%b/%y", "%d-%b-%y", "%d %b %Y"]
+    datetime_object = None
+
+    for format in valid_formats:
+        try:
+            datetime_object = datetime.strptime(date, format).date()
+            break
+        except ValueError:
+            pass
+    
+    if datetime_object != None and datetime_object > datetime.today().date():
+        error = "\n------Date cannot be in the future------"
+        return None, error
+
+    return datetime_object, error
+
+def validate_cost(cost):
+    if not cost.strip():   
+        type_text("\n----------Cost cannot be empty----------")
+        return False, None
+
+    try:
+        decimal_cost = Decimal(cost)
+    except:
+        type_text("\n-----------Invalid characters-----------")
+        return False, None
+    
+    if "." in cost:
+        split_cost = cost.split(".")
+
+        if len(split_cost[1]) > 2:
+            type_text("\n-------Invalid: Too many decimals-------")
+            return False, None
+
+    if decimal_cost < 0:
+        type_text("\n--------Cost cannot  be negative--------")
+        return False, None
+        
+    return True, decimal_cost
+
+def validate_category(category):
+    pass
